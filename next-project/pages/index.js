045a997862ee4a4fe75d3f50c51427597a4f3dea
Mikolajkusinski <77103765+Mikolajkusinski@users.noreply.github.com>
2022-10-06 14:01:32 +0200

    Project ready for deployment

---
+++ next-project/pages/index.js
@@ -0,0 +1,56 @@
+import { MongoClient } from "mongodb";
+import Head from "next/head";
+import { Fragment } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+function HomePage(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </Fragment>
+  );
+}
+
+// export async function getServerSideProps(context) {
+//     const req = context.req;
+//     const res = context.res;
+
+//   return {
+//     props: {
+//         meetups: DUMMY_MEETUPS
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://Miki:Miki1234@cluster0.2ixttvq.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
